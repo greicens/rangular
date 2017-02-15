82297594f6feb84045acc00bbeb73794428b6347
greicens <gsilva@mail.ccsf.edu>
2017-02-15 14:09:52 -0800

    adds api routes

--- rails_angular_sample/config/routes.rb
+++ rails_angular_sample/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'site#index'
+  namespace :api, defaults: { format: :json } do
+    resources :todos, except: [:new, :edit]
+  end
   get '*path', to: 'site#index'
 end
