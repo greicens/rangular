75eae62582021d6e0cc827b6ba1efd04e738b10a
greicens <gsilva@mail.ccsf.edu>
2017-02-15 13:58:39 -0800

    adds basic view

--- rails_angular_sample/app/assets/javascripts/app.js
+++ rails_angular_sample/app/assets/javascripts/app.js
@@ -1,1 +1,28 @@
-angular.module('sampleApp', ['ngRoute']);
+angular.module('sampleApp', ['ngRoute', 'templates'])
+  .config(config)
+  .controller('HomeIndexController', HomeIndexController);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config (  $routeProvider,   $locationProvider  )  {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'home.html',
+      controller: 'HomeIndexController',
+      controllerAs: 'homeIndexCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+  $locationProvider
+    .html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+};
+
+HomeIndexController.$inject=[];
+function HomeIndexController(){
+  var vm = this;
+  vm.greeting = "what's up?"
+}
