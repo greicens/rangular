15cf464c38c335920f9eeaf05a6eaf8b40bcd98f
johnson-rl <johnson.rl@gmail.com>
2017-02-15 18:19:02 -0800

    read and create

--- rails_angular_sample/app/assets/javascripts/app.js
+++ rails_angular_sample/app/assets/javascripts/app.js
@@ -21,8 +21,33 @@ function config (  $routeProvider,   $locationProvider  )  {
     });
 };
 
-HomeIndexController.$inject=[];
-function HomeIndexController(){
+HomeIndexController.$inject=['$http'];
+function HomeIndexController($http){
   var vm = this;
-  vm.greeting = "what's up?"
+  vm.greeting = "what's up?";
+  vm.newTodo = {};
+
+  $http({
+    method: 'GET',
+    url: '/api/todos'
+  }).then(function successCallback(response) {
+     console.log('response for get one', response);
+     vm.todos = response.data
+   }, function errorCallback(error) {
+     console.log('There was an error getting the data', error);
+   });
+
+   vm.createTodo = function (){
+     $http({
+       method: 'POST',
+       url: '/api/todos',
+       data: vm.newTodo
+     }).then(function successCallback(response) {
+       console.log('response for create todo:', response);
+       vm.todos.push(response.data);
+       vm.newTodo = null
+     }, function errorCallback(error) {
+       console.log('There was an creating the todo', error);
+     });
+   };
 }
